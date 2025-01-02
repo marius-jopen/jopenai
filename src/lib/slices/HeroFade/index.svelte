62c82316c6e0d3688cfe8f7bebaf1b7fcb9bc88e
Marius Jopen <mail@mariusjopen.com>
2025-01-02 21:45:21 +0100

    hero fade done

--- src/lib/slices/HeroFade/index.svelte
+++ src/lib/slices/HeroFade/index.svelte
@@ -3,7 +3,6 @@
 	import { PrismicImage } from '@prismicio/svelte';
 	import { Splide, SplideSlide } from '@splidejs/svelte-splide';
 	import '@splidejs/svelte-splide/css';
-	import { TypeWriter } from 'svelte-typewrite'
 
 	export let slice: Content.HeroFadeSlice;
 
@@ -39,7 +38,7 @@
 	>
 		{#each items as item, index}
 			<SplideSlide>
-				<a href="{item.link && item.link.url}" class="w-[90vw] md:w-[95vw] relative overflow-hidden">
+				<div on:click={goToNext} class="w-full relative cursor-pointer">
 					<div>
 						{#if item.video}
 							<video src={item.video} poster={item.image.url} class="object-cover w-full aspect-[3/4] lg:aspect-[16/8]" playsinline autoplay muted loop />
@@ -47,62 +46,12 @@
 							<PrismicImage class="object-cover w-full aspect-[3/4] lg:aspect-[16/8]" field={item.image} />
 						{/if}
 
-						<TypeWriter texts={['lorem ipsum', 'dolor sit amet']} />
-
-						<h1 data-aos="fade" class="text-white px-6 py-6 absolute bottom-0 left-0 w-full lg:w-3/4 h-full flex items-end">
+						<h1 data-aos="fade" class="text-white px-6 pb-7 absolute bottom-0 left-0 w-full lg:w-3/4 h-full flex items-end">
 							{item.text}
 						</h1>
 					</div>
-				</a>
+				</div>
 			</SplideSlide>
 		{/each}
 	</Splide>
-	<!-- Custom arrows outside the carousel -->
-	<div class="flex justify-center mt-4 ss-carousel__controls pt-2" data-aos="fade-up" data-aos-delay={500}>
-		<button on:click={goToPrev} class="ss-carousel__arrow disabled arrow-prev" aria-label="Previous"></button>
-		<button on:click={goToNext} class="ss-carousel__arrow arrow-next" aria-label="Next"></button>
-	</div>
 </section>
-
-
-<style lang="postcss">
-	.ss-carousel__controls {
-		display: grid;
-		grid-auto-flow: column;
-		grid-gap: 24px;
-	}
-
-	.ss-carousel__arrow {
-		padding: 0;
-		background: transparent;
-		box-shadow: none;
-		border: 0;
-		position: relative;
-		z-index: 10;
-		transform: scale(0.8);
-	}
-
-	.ss-carousel__arrow:before {
-		content: '';
-		background: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNSIgaGVpZ2h0PSI5IiB2aWV3Qm94PSIwIDAgMTUgOSI+Cgk8cGF0aCBmaWxsPSIjMzMzMzMzIiBkPSJNNy44NjcgOC41NzRsLTcuMjItNy4yMi43MDctLjcwOEw3Ljg2NyA3LjE2IDE0LjA1Ljk4bC43MDYuNzA3Ii8+Cjwvc3ZnPgo=');
-		background-size: contain;
-		filter: brightness(1);
-		display: block;
-		width: 38px;
-		height: 23px;
-		cursor: pointer;
-		transition: filter 0.5s ease-in-out;
-	}
-
-	.ss-carousel__arrow.arrow-prev:before {
-		transform: rotate(90deg);
-	}
-
-	.ss-carousel__arrow.arrow-next:before {
-		transform: rotate(-90deg);
-	}
-
-	.ss-carousel__arrow.disabled::before {
-		filter: brightness(4);
-	}
-</style>
